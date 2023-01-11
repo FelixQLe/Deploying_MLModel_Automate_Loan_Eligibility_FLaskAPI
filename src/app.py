809153eab23c9199e32d4ba6213d188e0b3ade6a
Hop Le <lequanghop844@gmail.com>
2023-01-10 22:51:54 -0500

    added pdf

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, jsonify,Flask
 import flask
+
+# We need load all the python packages we use in previous sections here
 import numpy as np
 import traceback #allows you to send error to user
 import pickle
@@ -17,23 +19,22 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import RidgeClassifier
-
+##############################
 
 
 # App definition
 app = Flask(__name__)
 
-#extract columns
+#extract columns as previous sections
+#because our Pickle will load pipeline models which uses this columns for feature transformations 
 label_cols = ['Gender','Married', 'Self_Employed', 'Education', 'Property_Area']
 log_scale_cols = ['LoanAmount', 'Total_Income']
 depep_cols = ['Dependents']
 loan_cols = ['Loan_Amount_Term']
 
-#Log function
+# The same reason, we also need all functions we define in previous sections as well in this app.py file
 def log_transform(x):
     return np.log(x + 1)
-
-# Labelencoder to Gender_transform, Married_transform, Self_Eployed_transform, Edu_transform(x):
     
 def Loan_term(X):
     X = X/12
@@ -56,11 +57,11 @@ class CustomLabelEncode(BaseEstimator, TransformerMixin):
             X[i]=le.fit_transform(X[i])
         return X
 
-# importing models
-with open('/Users/hople/Desktop/Bootcamp Lectures/week_7/mini-project-4/Model_Pickle/credit_classifier.pkl', 'rb') as f:
+# importing pickle file we saved , our predictive models was serialized in this file
+with open('/Users/hople/Desktop/Bootcamp Lectures/mini-project-4/Model_Pickle/credit_classifier.pkl', 'rb') as f:
    credit_predict = pickle.load(f)
 
-#webpage
+#webpage with two methods, POST and GET
 @app.route('/')
 def welcome():
    return "Welcome! Use this Flask App for Credit pre-determination"
@@ -69,7 +70,8 @@ def welcome():
 def predict():
 
    if flask.request.method == 'GET':
-       return "Prediction page. Try using post with params to get specific prediction. Format data to pandas dataframe"
+       return "Prediction page. Try using post with params to get specific prediction. \
+                                   Format data to pandas dataframe"
                 
 
    if flask.request.method == 'POST':
@@ -77,9 +79,9 @@ def predict():
            json_ = request.json # '_' since 'json' is a special word
            print(json_)
            query_pd = pd.DataFrame(json_)
-           #query = query_#.reindex(columns = model_columns, fill_value= 0)
            prediction = list(credit_predict.predict(query_pd))
-
+            
+           #return 0: No, 1, Yes
            return jsonify({
                "prediction":str(prediction)
            })
@@ -93,4 +95,3 @@ def predict():
 
 if __name__ == "__main__":
    app.run()
-
